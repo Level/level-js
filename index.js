dab0771ffe86576b6238c494aeb07674d0f30988
Rod Vagg <rod@vagg.org>
2013-05-04 14:55:53 +1000

    let abstract-leveldown do the hard validation work

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = Level
 
 var IDB = require('idb-wrapper')
+var AbstractLevelDOWN = require('abstract-leveldown').AbstractLevelDOWN
+var util = require('util')
 var Iterator = require('./iterator')
 
 function Level(location) {
@@ -10,14 +12,10 @@ function Level(location) {
   this.location = location
 }
 
-Level.prototype.open = function(options, callback) {
+util.inherits(Level, AbstractLevelDOWN)
+
+Level.prototype._open = function(options, callback) {
   var self = this
-  if (typeof options === 'function') {
-    callback = options
-    options = {}
-  }
-  if (!callback || typeof callback !== 'function') throw new Error("open() requires a callback argument")
-  
   this.idb = new IDB({
     storeName: this.location,
     autoIncrement: false,
@@ -29,17 +27,6 @@ Level.prototype.open = function(options, callback) {
       callback && callback(err)
     }
   })
-}
-
-Level.prototype.get = function (key, options, callback) {
-  if (typeof options == 'function') callback = options
-  if (typeof callback != 'function') throw new Error('get() requires a callback argument')
-  var err = checkKeyValue(key, 'key')
-  if (err) return callback(err)
-  if (!isBuffer(key)) key = String(key)
-  if (typeof options != 'object') options = {}
-  
-  return this._get(key, options, callback)
 }
 
 Level.prototype._get = function (key, options, callback) {
@@ -54,34 +41,8 @@ Level.prototype._get = function (key, options, callback) {
   }, callback)
 }
 
-Level.prototype.del = function (key, options, callback) {
-  if (typeof options == 'function') callback = options
-  if (typeof callback != 'function') throw new Error('del() requires a callback argument')
-  var err = checkKeyValue(key, 'key')
-  if (err) return callback(err)
-  if (!isBuffer(key)) key = String(key)
-  if (typeof options != 'object') options = {}
-  return this._del(key, options, callback)
-}
-
 Level.prototype._del = function(id, options, callback) {
   this.idb.remove(id, callback, callback)
-}
-
-Level.prototype.put = function (key, value, options, callback) {
-  if (typeof options == 'function')
-    callback = options
-  if (typeof callback != 'function')
-    throw new Error('put() requires a callback argument')
-  var err = checkKeyValue(value, 'value')
-  if (err) return callback(err)
-  err = checkKeyValue(key, 'key')
-  if (err) return callback(err)
-  if (!isBuffer(key)) key = String(key)
-  if (!isBuffer(value)) value = String(value)
-  if (typeof options != 'object') options = {}
-
-  return this._put(key, value, options, callback)
 }
 
 Level.prototype._put = function (key, value, options, callback) {
@@ -93,26 +54,8 @@ Level.prototype.iterator = function (options) {
   return new Iterator(this.idb, options)
 }
 
-Level.prototype.batch = function (array, options, callback) {
-  if (typeof options === 'function') callback = options
-  if (!Array.isArray(array) && typeof array === 'object') {
-    options = array
-    array = undefined
-  }
-  if (typeof options != 'object') options = {}
-
-  // TODO: if array == undefined && callback == function, derp
-
-  return this._batch(array, options, callback)
-}
-
 Level.prototype._batch = function (array, options, callback) {
   return this.idb.batch(array, function(){ callback() }, callback)
-}
-
-Level.prototype.close = function (callback) {
-  if (typeof callback != 'function') throw new Error('close() requires a callback argument')
-  return this._close(callback)
 }
 
 Level.prototype._close = function (callback) {
@@ -120,8 +63,25 @@ Level.prototype._close = function (callback) {
   callback()
 }
 
-function isBuffer(buf) {
+Level.prototype._approximateSize = function() {
+  throw new Error('Not implemented')
+}
+
+var isBuffer = Level.prototype._isBuffer = function (buf) {
   return buf instanceof ArrayBuffer
+}
+
+var checkKeyValue = Level.prototype._checkKeyValue = function (obj, type) {
+  if (obj === null || obj === undefined)
+    return new Error(type + ' cannot be `null` or `undefined`')
+  if (obj === null || obj === undefined)
+    return new Error(type + ' cannot be `null` or `undefined`')
+  if (isBuffer(obj) && obj.byteLength === 0)
+    return new Error(type + ' cannot be an empty ArrayBuffer')
+  if (String(obj) === '')
+    return new Error(type + ' cannot be an empty String')
+  if (obj.length === 0)
+    return new Error(type + ' cannot be an empty Array')
 }
 
 function ArrayBufferToString(buf) {
@@ -135,17 +95,4 @@ function StringToArrayBuffer(str) {
     bufView[i] = str.charCodeAt(i)
   }
   return buf
-}
-
-function checkKeyValue (obj, type) {
-  if (obj === null || obj === undefined)
-    return new Error(type + ' cannot be `null` or `undefined`')
-  if (obj === null || obj === undefined)
-    return new Error(type + ' cannot be `null` or `undefined`')
-  if (isBuffer(obj) && obj.byteLength === 0)
-    return new Error(type + ' cannot be an empty ArrayBuffer')
-  if (String(obj) === '')
-    return new Error(type + ' cannot be an empty String')
-  if (obj.length === 0)
-    return new Error(type + ' cannot be an empty Array')
 }
