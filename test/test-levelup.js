cf5621efb187f58d7eb04b6fc6da324da86ee301
Vincent Weevers <mail@vincentweevers.nl>
2018-05-23 16:40:15 +0200

    make real tests out of test-levelup.js

--- test/test-levelup.js
+++ test/test-levelup.js
@@ -1,25 +1,64 @@
 /*** Levelup tests
-  (the actual test suite isnt runnable in browser, and these arent complete)
+  Temporary to test integration, can be removed later.
 ***/
 var levelup = require('levelup')
 var leveljs = require('../')
+var test = require('tape')
+var testCommon = require('./testCommon')
 
-window.db = levelup('foo', { db: leveljs })
+function identity (v) {
+  return v
+}
 
-db.put('name', 'LevelUP string', function (err) {
-  if (err) return console.log('Ooops!', err) // some kind of I/O error
-  db.get('name', function (err, value) {
-    if (err) return console.log('Ooops!', err) // likely the key was not found
-    console.log('name=' + value)
+test('setup', testCommon.setUp)
+
+test('levelup put', function (t) {
+  t.plan(4)
+
+  // TODO: update signature after upgrading levelup
+  var db = levelup(testCommon.location(), { db: leveljs })
+
+  db.put('name', 'LevelUP string', function (err) {
+    t.ifError(err, 'no put error')
+
+    db.get('name', function (err, value) {
+      t.ifError(err, 'no get error')
+      t.is(value, 'LevelUP string')
+
+      db.close(function (err) {
+        t.ifError(err, 'no close error')
+      })
+    })
   })
 })
 
-var ary = new Uint8Array(1)
-ary[0] = 1
-db.put('binary', ary, function (err) {
-  if (err) return console.log('Ooops!', err) // some kind of I/O error
-  db.get('binary', function (err, value) {
-    if (err) return console.log('Ooops!', err) // likely the key was not found
-    console.log('binary', value)
+test('binary', function (t) {
+  t.plan(6)
+
+  // TODO: update signature after upgrading levelup
+  var db = levelup(testCommon.location(), { db: leveljs, valueEncoding: 'binary' })
+  var buf = Buffer.from('00ff', 'hex')
+
+  db.put('binary', buf, function (err) {
+    t.ifError(err, 'no put error')
+
+    db.get('binary', function (err, value) {
+      t.ifError(err, 'no get error')
+
+      // This levelup is really old and its binary decoder does:
+      // `return process.browser ? buffer.toString(type) : buffer`
+      t.notOk(Buffer.isBuffer(value), 'not a buffer')
+
+      db.get('binary', { valueEncoding: { decode: identity } }, function (err, value) {
+        t.ifError(err, 'no get error')
+        t.ok(Buffer.isBuffer(value), 'is a buffer')
+
+        db.close(function (err) {
+          t.ifError(err, 'no close error')
+        })
+      })
+    })
   })
 })
+
+test('teardown', testCommon.tearDown)
