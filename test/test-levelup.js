c7bc3b484f15140cf5f12e81d458617415670fdc
Vincent Weevers <mail@vincentweevers.nl>
2018-05-24 14:44:51 +0200

    fix levelup integration tests

--- test/test-levelup.js
+++ test/test-levelup.js
@@ -1,27 +1,25 @@
 /*** Levelup tests
-  Temporary to test integration, can be removed later.
+  NOTE: Temporary integration tests, can be removed later. Remember
+  to also remove the encoding-down devDependency.
 ***/
 var levelup = require('levelup')
+var encoding = require('encoding-down')
 var leveljs = require('../')
 var test = require('tape')
 var testCommon = require('./testCommon')
-
-function identity (v) {
-  return v
-}
 
 test('setup', testCommon.setUp)
 
 test('levelup put', function (t) {
   t.plan(4)
 
-  // TODO: update signature after upgrading levelup
-  var db = levelup(testCommon.location(), { db: leveljs })
+  var down = leveljs(testCommon.location())
+  var db = levelup(down)
 
   db.put('name', 'LevelUP string', function (err) {
     t.ifError(err, 'no put error')
 
-    db.get('name', function (err, value) {
+    db.get('name', { asBuffer: false }, function (err, value) {
       t.ifError(err, 'no get error')
       t.is(value, 'LevelUP string')
 
@@ -33,10 +31,10 @@ test('levelup put', function (t) {
 })
 
 test('binary', function (t) {
-  t.plan(6)
+  t.plan(9)
 
-  // TODO: update signature after upgrading levelup
-  var db = levelup(testCommon.location(), { db: leveljs, valueEncoding: 'binary' })
+  var down = leveljs(testCommon.location())
+  var db = levelup(encoding(down, { valueEncoding: 'binary' }))
   var buf = Buffer.from('00ff', 'hex')
 
   db.put('binary', buf, function (err) {
@@ -44,14 +42,14 @@ test('binary', function (t) {
 
     db.get('binary', function (err, value) {
       t.ifError(err, 'no get error')
+      t.ok(Buffer.isBuffer(value), 'is a buffer')
+      t.same(value, buf)
 
-      // This levelup is really old and its binary decoder does:
-      // `return process.browser ? buffer.toString(type) : buffer`
-      t.notOk(Buffer.isBuffer(value), 'not a buffer')
-
-      db.get('binary', { valueEncoding: { decode: identity } }, function (err, value) {
+      db.get('binary', { valueEncoding: 'id' }, function (err, value) {
         t.ifError(err, 'no get error')
-        t.ok(Buffer.isBuffer(value), 'is a buffer')
+        t.notOk(Buffer.isBuffer(value), 'is not a buffer')
+        t.ok(value instanceof Uint8Array, 'is a Uint8Array')
+        t.same(Buffer.from(value), buf)
 
         db.close(function (err) {
           t.ifError(err, 'no close error')
