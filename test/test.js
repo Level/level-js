f5171a803c0cfb374d1b8abecd7c1db9b8e154df
Vincent Weevers <mail@vincentweevers.nl>
2018-05-24 13:31:41 +0200

    remove testBuffer

--- test/test.js
+++ test/test.js
@@ -5,15 +5,13 @@ var testCommon = require('./testCommon')
 // load IndexedDBShim in the tests
 require('./idb-shim.js')()
 
-var testBuffer = Buffer.from('foo')
-
 /*** compatibility with basic LevelDOWN API ***/
 require('abstract-leveldown/abstract/leveldown-test').args(leveljs, tape, testCommon)
 require('abstract-leveldown/abstract/open-test').open(leveljs, tape, testCommon)
 require('abstract-leveldown/abstract/put-test').all(leveljs, tape, testCommon)
 require('abstract-leveldown/abstract/del-test').all(leveljs, tape, testCommon)
 require('abstract-leveldown/abstract/get-test').all(leveljs, tape, testCommon)
-require('abstract-leveldown/abstract/put-get-del-test').all(leveljs, tape, testCommon, testBuffer)
+require('abstract-leveldown/abstract/put-get-del-test').all(leveljs, tape, testCommon)
 require('abstract-leveldown/abstract/batch-test').all(leveljs, tape, testCommon)
 require('abstract-leveldown/abstract/chained-batch-test').all(leveljs, tape, testCommon)
 require('abstract-leveldown/abstract/close-test').close(leveljs, tape, testCommon)
