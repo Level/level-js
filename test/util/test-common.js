b9f5cbbaff06e02227a76c631bdffa094d194e7c
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-05-26 22:16:41 +0200

    split initialized var declarations into multiple statements

--- test/util/test-common.js
+++ test/util/test-common.js
@@ -1,86 +1,83 @@
-var dbidx = 0,
-  leveljs = require('../..'),
-  location = function () {
-    return '_leveldown_test_db_' + dbidx++
-  },
+var dbidx = 0
+var leveljs = require('../..')
+var location = function () {
+  return '_leveldown_test_db_' + dbidx++
+}
 
-  lastLocation = function () {
-    return '_leveldown_test_db_' + dbidx
-  },
+var lastLocation = function () {
+  return '_leveldown_test_db_' + dbidx
+}
 
-  cleanup = function (callback) {
-    var list = []
-    if (dbidx === 0) return callback()
-    for (var i = 0; i < dbidx; i++) {
-      list.push('_leveldown_test_db_' + i)
-    }
+var cleanup = function (callback) {
+  var list = []
+  if (dbidx === 0) return callback()
+  for (var i = 0; i < dbidx; i++) {
+    list.push('_leveldown_test_db_' + i)
+  }
 
-    function destroy () {
-      var f = list.pop()
-      if (list.length === 0) return callback()
-      leveljs.destroy(f, destroy)
-    }
+  function destroy () {
+    var f = list.pop()
+    if (list.length === 0) return callback()
+    leveljs.destroy(f, destroy)
+  }
 
-    destroy()
-  },
+  destroy()
+}
 
-  setUp = function (t) {
-    cleanup(function (err) {
-      t.notOk(err, 'cleanup returned an error')
-      t.end()
-    })
-  },
+var setUp = function (t) {
+  cleanup(function (err) {
+    t.notOk(err, 'cleanup returned an error')
+    t.end()
+  })
+}
 
-  tearDown = function (t) {
-    setUp(t) // same cleanup!
-  },
+var tearDown = function (t) {
+  setUp(t) // same cleanup!
+}
 
-  collectEntries = function (iterator, callback) {
-    var data = [],
-      next = function () {
-        iterator.next(function (err, key, value) {
-          if (err) return callback(err)
-          if (!arguments.length) {
-            return iterator.end(function (err) {
-              callback(err, data)
-            })
-          }
-          data.push({ key: key, value: value })
-          process.nextTick(next)
+var collectEntries = function (iterator, callback) {
+  var data = []
+  var next = function () {
+    iterator.next(function (err, key, value) {
+      if (err) return callback(err)
+      if (!arguments.length) {
+        return iterator.end(function (err) {
+          callback(err, data)
         })
       }
-    next()
-  },
+      data.push({ key: key, value: value })
+      process.nextTick(next)
+    })
+  }
+  next()
+}
 
-  makeExistingDbTest = function (name, test, leveldown, testFn) {
-    test(name, function (t) {
-      cleanup(function () {
-        var loc = location(),
-          db = leveldown(loc),
-          done = function (close) {
-            if (close === false) { return cleanup(t.end.bind(t)) }
-            db.close(function (err) {
-              t.notOk(err, 'no error from close()')
-              cleanup(t.end.bind(t))
-            })
-          }
-        db.open(function (err) {
-          t.notOk(err, 'no error from open()')
-          db.batch(
-            [
-              { type: 'put', key: 'one', value: '1' },
-              { type: 'put', key: 'two', value: '2' },
-              { type: 'put', key: 'three', value: '3' }
-            ]
-            , function (err) {
-              t.notOk(err, 'no error from batch()')
-              testFn(db, t, done, loc)
-            }
-          )
+var makeExistingDbTest = function (name, test, leveldown, testFn) {
+  test(name, function (t) {
+    cleanup(function () {
+      var loc = location()
+      var db = leveldown(loc)
+      var done = function (close) {
+        if (close === false) { return cleanup(t.end.bind(t)) }
+        db.close(function (err) {
+          t.notOk(err, 'no error from close()')
+          cleanup(t.end.bind(t))
+        })
+      }
+      db.open(function (err) {
+        t.notOk(err, 'no error from open()')
+        db.batch([
+          { type: 'put', key: 'one', value: '1' },
+          { type: 'put', key: 'two', value: '2' },
+          { type: 'put', key: 'three', value: '3' }
+        ], function (err) {
+          t.notOk(err, 'no error from batch()')
+          testFn(db, t, done, loc)
         })
       })
     })
-  }
+  })
+}
 
 module.exports = {
   location: location,
